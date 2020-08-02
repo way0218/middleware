7340fb18991db4a8b4944b79e06d6e4d6bf342d4
Vexth <oo932758@sina.com>
2020-08-02 17:42:37 +0800

    middleware

--- middleware.go
+++ middleware.go
@@ -7,19 +7,19 @@ type Middleware struct {
 }
 
 // New 初始化Middleware
-func New() Middleware {
-	return Middleware{}
+func New() *Middleware {
+	return &Middleware{}
 }
 
 // 注册中间件
-func (m Middleware) use(f ...func(options interface{}, next func(interface{}))) Middleware {
+func (m *Middleware) use(f ...func(options interface{}, next func(interface{}))) *Middleware {
 	m.Cache = append(m.Cache, f...)
 	return m
 }
 
 // 每个中间件只有两个形参 第一是传进来的参数 第二个是调用下一个中间件的函数
 // 中间件的执行顺序是根据你注册中间件的顺序来去调用的
-func (m Middleware) next(params interface{}) {
+func (m *Middleware) next(params interface{}) {
 	if len(m.Middlewares) == 0 {
 		return
 	}
@@ -28,7 +28,7 @@ func (m Middleware) next(params interface{}) {
 	ware(params, m.next)
 }
 
-func (m Middleware) execute(params interface{}) {
+func (m *Middleware) execute(params interface{}) {
 	m.Middlewares = append(m.Middlewares, m.Cache...) // 复制一份
 	m.next(params)
 }
